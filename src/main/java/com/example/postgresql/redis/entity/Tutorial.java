4089e1ce2d64b57c72a8fb026a188ac6ea0666af
piyushprasad2007 <piyushprasad2007@gmail.com>
2023-04-16 16:55:03 +0530

    working crud operations

---
+++ src/main/java/com/example/postgresql/redis/entity/Tutorial.java
@@ -0,0 +1,38 @@
+package com.example.postgresql.redis.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.io.Serializable;
+
+@Setter
+@Getter
+@Table
+@AllArgsConstructor
+@NoArgsConstructor
+public class Tutorial implements Serializable {
+
+    @Id
+    private int id;
+
+    private String title;
+
+    private String description;
+
+    private boolean published;
+
+    public Tutorial(String title, String description, boolean published) {
+        this.title = title;
+        this.description = description;
+        this.published = published;
+    }
+
+    @Override
+    public String toString() {
+        return "Tutorial [id=" + id + ", title=" + title + ", desc=" + description + ", published=" + published + "]";
+    }
+}
---
+++ src/test/java/com/example/postgresql/redis/ReactivePostgresqlRedisAppTests.java
@@ -0,0 +1,13 @@
+package com.example.postgresql.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactivePostgresqlRedisAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
